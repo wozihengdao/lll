4b727114dc9be2fae664a978994c009e5251fdb7
wozihengdao <142223252+wozihengdao@users.noreply.github.com>
2023-08-26 22:04:57 +0800

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 1;
+	int b = 3;
+	if (a == 1)
+	
+		if (b == 2)
+			printf("jjjjj\n");
+		else
+
+			printf("bbbbb\n");
+	
+	else
+		printf("kkkkkkk/n");
+	return 0;
+}
+//int main()
+//{
+//	int a = 0;
+//	int b = 2;
+//	if (a = 1)
+//		if£¨b = 2£©
+//		printf("6666\n");
+//	printf("hhh\n");
+//	else
+//		printf("123456\n");
+//	return 0; int main()
+//	{
+//		int a = 0;
+//		int b = 2;
+//		if (a = 1)
+//			if£¨b = 2£©
+//			printf("6666\n");
+//		printf("hhh\n");
+//		else
+//			printf("123456\n");
+//		return 0; int main()
+//		{
+//			int a = 0;
+//			int b = 2;
+//			if (a = 1)
+//				if£¨b = 2£©
+//				printf("6666\n");
+//			printf("hhh\n");
+//			else
+//				printf("123456\n");
+//			return 0; int main()
+//			{
+//				int a = 0;
+//				int b = 2;
+//				if (a = 1)
+//					if£¨b = 2£©
+//					printf("6666\n");
+//				printf("hhh\n");
+//				else
+//					printf("123456\n");
+//				return 0; int main()
+//				{
+//					int a = 0;
+//					int b = 2;
+//					if (a = 1)
+//						if£¨b = 2£©
+//						printf("6666\n");
+//					printf("hhh\n");
+//					else
+//						printf("123456\n");
+//					return 0; int main()
+//					{
+//						int a = 0;
+//						int b = 2;
+//						if (a = 1)
+//							if£¨b = 2£©
+//							printf("6666\n");
+//						printf("hhh\n");
+//						else
+							
