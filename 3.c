5febf665b0cbddc9d8965f6e5a63c62ec93f259c
wozihengdao <142223252+wozihengdao@users.noreply.github.com>
2023-08-27 17:02:02 +0800

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,21 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int main()
-{
-	int a = 1;
-	int b = 3;
-	if (a == 1)
-	
-		if (b == 2)
-			printf("jjjjj\n");
-		else
-
-			printf("bbbbb\n");
-	
-	else
-		printf("kkkkkkk/n");
-	return 0;
-}
+//int main()
+//{
+//	int a = 1;
+//	int b = 3;
+//	if (a == 1)
+//	
+//		if (b == 2)
+//			printf("jjjjj\n");
+//		else
+//
+//			printf("bbbbb\n");
+//	
+//	else
+//		printf("kkkkkkk/n");
+//	return 0;
+//}
 //int main()
 //{
 //	int a = 0;
@@ -75,4 +75,47 @@ int main()
 //							printf("6666\n");
 //						printf("hhh\n");
 //						else
-							
+//int main()
+//{
+//	int a = 3;
+//	if (a == 3)
+//	{
+//		printf("derderder/n");
+//	}
+//	return 0;
+//			}
+//int main()写数字判断是否为奇数
+//{
+//	int a = 2;
+//	if (a % 2 == 1)
+//		printf("yes/n");
+//	else
+//		printf("no/n");
+//	return 0;
+//}
+//奇数写法int main
+
+
+
+//int main()
+//{ 
+//	int i = 1;
+//
+//	while (i <= 100）
+//		{
+//			if (i%2 != 0)
+//			printf("%d",i);
+//		i++;
+//		}
+//	return 0;
+////}
+//int main()
+//{ 
+//	int a = 1;
+//	while(100>=a)
+//	{
+//		printf("%d", a);
+//		a += 2;
+//	}
+//	return 0;
+//}
