e2ba3f4f335d3445271926983ada8587f955d910
wozihengdao <142223252+wozihengdao@users.noreply.github.com>
2023-09-03 15:42:54 +0800

    Add files via upload

--- 3.c
+++ 3.c
@@ -115,7 +115,69 @@
 //	while(100>=a)
 //	{
 //		printf("%d", a);
-//		a += 2;
+//		a += 2;:
+//	}
+//	return 0;
+
+//}hao
+#pragma warning(disable:6031)
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d",&day);
+//	switch(day)
+//	{
+//	case 1+8: 
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期日\n");
+//		break;
+//	default:
+//		printf("输入错误\n")
+//		break;
+//}
+//	return 0;
+//}
+//int main()
+//{k
+//	int a = 1; 
+//	int b = 4;
+//	printf("%d=a,%d=b", a,b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	while (a <= 10)
+//	{
+//		printf("%d", a);
+//		a++;
+//	}
+//	return 0;
+//}
+//int m
+//int main()
+//{
+//	int ch =0 ;
+//	while((ch=getchar())!=EOF)
+//	{
+//		putchar(ch);
 //	}
 //	return 0;
 //}
